e0d8f756a992fc1b04633410a2077ce48e2ecae1
James <Grimkey@gmail.com>
2017-08-14 19:30:08 -0700

    Add project files.

---
+++ LogicalClock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
